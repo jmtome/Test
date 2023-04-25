f17954ef80b062814f47a26cdb03c6b8a519d5d4
jmtome <jtome@fi.uba.ar>
2023-04-25 16:59:54 -0300

    test
    test

---
+++ GHFollowers/GHFollowers/Custom Views/Cells/FollowerCell/FollowerView.swift
@@ -0,0 +1,40 @@
+//
+//  FollowerView.swift
+//  GHFollowers
+//
+//  Created by Sean Allen on 10/17/22.
+//  Copyright © 2022 Sean Allen. All rights reserved.
+//
+
+import SwiftUI
+
+struct FollowerView: View {
+
+    var follower: Follower
+
+    var body: some View {
+        VStack {
+            AsyncImage(url: URL(string: follower.avatarUrl)) { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            } placeholder: {
+                Image("avatar-placeholder")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }
+            .clipShape(Circle())
+
+            Text(follower.login)
+                .bold()
+                .lineLimit(1)
+                .minimumScaleFactor(0.6)
+        }
+    }
+}
+
+struct FollowerView_Previews: PreviewProvider {
+    static var previews: some View {
+        FollowerView(follower: Follower(login: "SeanAllen", avatarUrl: ""))
+    }
+}
